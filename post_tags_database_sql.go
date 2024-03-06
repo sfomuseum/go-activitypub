b460681ad8ce65ecdc2f9c5e81a280eb40908df1
sfomuseumbot <sfomuseumbot@localhost>
2024-03-06 14:49:50 -0800

    snapshot: finish implementing post tags and aliases for SQL

--- post_tags_database_sql.go
+++ post_tags_database_sql.go
@@ -59,6 +59,53 @@ func NewSQLPostTagsDatabase(ctx context.Context, uri string) (PostTagsDatabase, 
 }
 
 func (db *SQLPostTagsDatabase) GetPostTagIdsForDateRange(ctx context.Context, start int64, end int64, cb GetPostTagIdsCallbackFunc) error {
+
+	pg_callback := func(pg_rsp pg_sql.PaginatedResponse) error {
+
+		rows := pg_rsp.Rows()
+
+		for rows.Next() {
+
+			var id int64
+
+			err := rows.Scan(&id)
+
+			if err != nil {
+				return fmt.Errorf("Failed to query database, %w", err)
+			}
+
+			err = cb(ctx, id)
+
+			if err != nil {
+				return fmt.Errorf("Failed to execute following callback for post tag %d, %w", id, err)
+			}
+
+			return nil
+		}
+
+		err := rows.Close()
+
+		if err != nil {
+			return fmt.Errorf("Failed to iterate through database rows, %w", err)
+		}
+
+		return nil
+	}
+
+	pg_opts, err := countable.NewCountableOptions()
+
+	if err != nil {
+		return fmt.Errorf("Failed to create pagination options, %w", err)
+	}
+
+	q := fmt.Sprintf("SELECT id FROM %s WHERE created >= ? AND created <= ?", SQL_POST_TAGS_TABLE_NAME)
+
+	err = pg_sql.QueryPaginatedAll(db.database, pg_opts, pg_callback, q, start, end)
+
+	if err != nil {
+		return fmt.Errorf("Failed to execute paginated query, %w", err)
+	}
+
 	return nil
 }
 
