5e3903640d0a3fcd56e442ea8f0fc30a24fcc947
sfomuseumbot <sfomuseumbot@localhost>
2024-10-10 14:58:40 -0700

    trigger panic creating post

---
+++ cmd/create-post/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/sfomuseum/go-activitypub/app/post/create"
+)
+
+func main() {
+
+	ctx := context.Background()
+	err := create.Run(ctx)
+
+	if err != nil {
+		log.Fatalf("Failed to create post, %v", err)
+	}
+}
