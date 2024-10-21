a6ac23243c561178b3c30ce300d809278fe7ba86
sfomuseumbot <sfomuseumbot@localhost>
2024-10-21 14:01:38 -0700

    fleshing out delivery sql methods

--- database/deliveries_database_sql.go
+++ database/deliveries_database_sql.go
@@ -60,19 +60,29 @@ func NewSQLDeliveriesDatabase(ctx context.Context, uri string) (DeliveriesDataba
 }
 
 func (db *SQLDeliveriesDatabase) AddDelivery(ctx context.Context, d *activitypub.Delivery) error {
+
+	q := fmt.Sprintf("INSERT INTO %s (id, activity_id, activitypub_id, account_id, recipient, inbox, created, completed, success, error) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", SQL_DELIVERIES_TABLE_NAME)
+
+	_, err := db.database.ExecContext(ctx, q, d.Id, d.ActivityId, d.ActivityPubId, d.AccountId, d.Recipient, d.Inbox, d.Created, d.Completed, d.Success, d.Error)
+
+	if err != nil {
+		return fmt.Errorf("Failed to add delivery, %w", err)
+	}
+
 	return nil
 }
 
 func (db *SQLDeliveriesDatabase) GetDeliveryWithId(ctx context.Context, id int64) (*activitypub.Delivery, error) {
-	return nil, activitypub.ErrNotFound
+	where := "id = ?"
+	return db.getDelivery(ctx, where, id)
 }
 
 func (db *SQLDeliveriesDatabase) GetDeliveriesWithActivityIdAndRecipient(ctx context.Context, activity_id int64, recipient string, cb GetDeliveriesCallbackFunc) error {
-	return nil
+	// FIX ME
 }
 
-func (db *SQLDeliveriesDatabase) GetDeliveriesWithActivityPubIdAndRecipient(ctx context.Context, activity_pub_id string, recipient string, cb GetDeliveriesCallbackFunc) error {
-	return nil
+func (db *SQLDeliveriesDatabase) GetDeliveriesWithActivityPubIdAndRecipient(ctx context.Context, activitypub_id string, recipient string, cb GetDeliveriesCallbackFunc) error {
+	// FIX ME
 }
 
 func (db *SQLDeliveriesDatabase) GetDeliveryIdsForDateRange(ctx context.Context, start int64, end int64, cb GetDeliveryIdsCallbackFunc) error {
@@ -128,4 +138,52 @@ func (db *SQLDeliveriesDatabase) GetDeliveryIdsForDateRange(ctx context.Context,
 
 func (db *SQLDeliveriesDatabase) Close(ctx context.Context) error {
 	return db.database.Close()
+}
+
+func (db *SQLDeliveriesDatabase) getDelivery(ctx context.Context, where string, args ...interface{}) (*activitypub.Delivery, error) {
+
+	var id int64
+	var activity_id int64
+	var activitypub_id string
+	var account_id int64
+	var recipient string
+	var inbox string
+	var created int64
+	var completed int64
+	var success_i int
+	var error string
+
+	q := fmt.Sprintf("SELECT id, activity_id, activitypub_id, account_id, recipient, inbox, created, completed, success, error FROM %s WHERE %s", SQL_DELIVERIES_TABLE_NAME, where)
+
+	row := db.database.QueryRowContext(ctx, q, args...)
+
+	err := row.Scan(&id, &activity_id, &activitypub_id, &account_id, &recipient, &inbox, &created, &completed, &success_i, &error)
+
+	switch {
+	case err == sql.ErrNoRows:
+		return nil, activitypub.ErrNotFound
+	case err != nil:
+		return nil, err
+	default:
+		//
+	}
+
+	a := &activitypub.Delivery{
+		Id:            id,
+		ActivityId:    activity_id,
+		ActivityPubId: activitypub_id,
+		AccountId:     account_id,
+		Recipient:     recipient,
+		Inbox:         inbox,
+		Created:       created,
+		Completed:     completed,
+		Error:         error,
+	}
+
+	if success_i > 0 {
+		a.Success = true
+	}
+
+	return a, nil
+
 }
