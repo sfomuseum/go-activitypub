adf72d82cf08e3deb512e7e7a833b96094c16f42
thisisaaronland <thisisaaronland@localhost>
2026-02-11 12:06:02 -0800

    block out generic query stuff

--- database/database.go
+++ database/database.go
@@ -13,12 +13,35 @@ import (
 // to use this and {FOO}_{BAR}Database to implement the {FOO}
 // interface.
 
+type Condition struct {
+	Field     string `json:"field"`
+	Operation string `json:"operation"`
+	Value     any    `json:"value"`
+}
+
+type Where struct {
+	Conditions []*Condition `json:"conditions"`
+	Relation   string       `json:"relation"`
+}
+
+type OrderBy struct {
+	Field     string `json:"field"`
+	Direction string `json:"direction"`
+}
+
+type Query struct {
+	Where   *Where   `json:"where"`
+	Offset  int      `json:"offset"`
+	Limit   int      `json:"limit"`
+	OrderBy *OrderBy `json:"order_by"`
+}
+
 type Database[T any] interface {
 	AddRecord(context.Context, T) error
 	RemoveRecord(context.Context, T) error
 	UpdateRecord(context.Context, T) error
 	GetRecord(context.Context, int64) (T, error)
-	ListRecords(context.Context) iter.Seq2[T, error]
+	QueryRecords(context.Context, *Query) iter.Seq2[T, error]
 	Close() error
 }
 
@@ -28,7 +51,7 @@ func Migrate[T any](ctx context.Context, src Database[T], dst Database[T]) (int6
 	success := int64(0)
 	errors := int64(0)
 
-	for v, err := range src.ListRecords(ctx) {
+	for v, err := range src.QueryRecords(ctx, nil) {
 
 		defer atomic.AddInt64(&count, 1)
 
